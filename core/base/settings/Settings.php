6cbe962c9269654d133c631a74dd4a717dbc8851
Alexandr <i9312452497@gmail.com>
2021-11-19 15:28:55 +0300

    less5

--- core/base/settings/Settings.php
+++ core/base/settings/Settings.php
@@ -5,5 +5,41 @@ namespace base\settings;
 class Settings
 {
 
-    private $routes = [];
+    private $routes = [
+        'admin' => [
+            'name' => 'admin',
+            'path' => 'core/admin/controllers',
+            'hrUrl' => 'false'
+        ],
+        'settings' => [
+            'path' => 'core/base/settings/'
+        ],
+        'plagins' => [
+            'path' => 'core/plagins/',
+            'hrUrl' => 'false'
+        ],
+        'user' => [
+            'path' => 'core/user/',
+            'hrUrl' => 'true',
+            'routes' => []
+        ],
+        'default' => [
+            'controller' => 'IndexController',
+            'inputMethod' => 'inputData',
+            'outputMethod' => 'outputData'
+        ]
+
+    ];
+    private function __construct()
+    {
+    }
+    private function __clone()
+    {
+    }
+    static private function instance(){
+        if(self::$_instance instanceof self){
+            return self::$_instance;
+        }
+        return self::$_instance new self;
+    }
 }
